8988fd93eab1f7e4fe8b17c8dca0a577a4238ebc
renatof4rias <renatof4rias@gmail.com>
2023-08-03 12:57:33 -0300

    Entidade USER criada

---
+++ Spring-1/src/main/java/com/softwarek/Spring1/entities/User.java
@@ -0,0 +1,99 @@
+package com.softwarek.Spring1.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name= "tb_users")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	public interface CreateUser{}
+	
+	public interface UpdateUser{}
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name= "id", unique = true)
+	private Long id;
+	
+	@Column(name= "username", length = 100, nullable = false, unique = true)
+	@NotNull(groups = CreateUser.class)
+	@NotEmpty(groups = CreateUser.class)
+	@Size(groups = CreateUser.class, min = 2, max = 100)
+	private String username;
+	
+	@Column(name= "password", length = 60, nullable = false)
+	@NotNull(groups = {CreateUser.class, UpdateUser.class})
+	@NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+	@Size(groups = {CreateUser.class, UpdateUser.class}, min = 2, max = 60)
+	private String password;
+	
+	//private List<Task> task = new ArrayList<Task>();
+	
+	public User() {
+	}
+
+	public User(Long id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, password, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(username, other.username);
+	}
+	
+}
